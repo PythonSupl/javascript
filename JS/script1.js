6fa1d8f34c510096e6886396fe4f86c1e68d0e22
Z_R_K <auzyaksuplbiz@gmail.com>
2022-04-29 20:18:07 +0700

    KFC

---
+++ JS/script1.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//const num = prompt('Твое любимое число?', '');
+
+//if (num < 49) {
+//    console.log('Error');
+//} else if (num > 100) {
+//    alert('Mnogo');
+//} else {
+//    console.log('Ok!')
+//}
+ // тернарный оператор
+// (num == 50) ? console.log('Ok!') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Net');
+        break;
+    case 100:
+        console.log('Net');
+        break;
+    case 50:
+        console.log('Aga');
+        break;
+    default:
+        console.log('ne v etot raz');
+        break;
+}
