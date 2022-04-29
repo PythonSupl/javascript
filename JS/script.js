600156bcb167ffe979d930d6bca1e6be14a4002d
Z_R_K <auzyaksuplbiz@gmail.com>
2022-04-29 17:42:16 +0700

    film lover

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,21 @@
-alert("fdsf");
+const numberOfFilms = +prompt('How many films do you watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('What is the last films do you watched?', ''),
+      b = prompt('Your grade?', ''),
+      c = prompt('What is the last films do you watched?', ''),
+      d = prompt('Your grade?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+alert(personalMovieDB);
